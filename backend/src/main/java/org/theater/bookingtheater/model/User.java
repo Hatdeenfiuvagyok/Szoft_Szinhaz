f79f74e1e03bcb9f4052764733fb94230e6a116c
JkbAronx1 <aron.jk76@gmail.com>
2025-10-21 15:29:09 +0200

    felhasználó email/jelszó hash

---
+++ backend/src/main/java/org/theater/bookingtheater/model/User.java
@@ -0,0 +1,23 @@
+package org.theater.bookingtheater.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+}
